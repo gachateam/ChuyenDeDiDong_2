1a22a9b85f0ec62ea5a66f8a5e1397732f28c77e
baobaote00 <baobaote00@gmail.com>
2021-10-19 21:39:44 +0700

    Update ImgQuestionsScreen.js

--- screens/ImgQuestionsScreen.js
+++ screens/ImgQuestionsScreen.js
@@ -1,76 +1,63 @@
 import React from 'react'
-import { Image, ImageBackground, ImageEditor, StyleSheet, Text, View , TouchableOpacity} from 'react-native'
+import { Image, ImageBackground, ImageEditor, StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native'
 
+const height = Dimensions.get('screen').height
 
+const ImgQuestionsScreen = ({ navigate }) => {
 
-const ImgQuestionsScreen = ({navigate}) => {
     return (
         <View style={styles.container}>
-            <View style= {styles.top}>
-                <Text style = {styles.questions}>
-                    Question
-                </Text>
+            <View style={styles.top}>
+                <Text style={styles.questions}>Question</Text>
             </View>
             <View style={styles.options}>
                 <TouchableOpacity style={styles.optionButton}>
-                <Image 
-                    source = {{
-                        uri:'https://cdni.iconscout.com/illustration/premium/thumb/giving-different-feedback-and-review-in-websites-2112230-1779230.png',
-                    
-                    }} 
-                    style = {styles.banner}
-                    resizeMode = "contain"
+                    <Image
+                        source={{
+                            uri: 'https://cdni.iconscout.com/illustration/premium/thumb/giving-different-feedback-and-review-in-websites-2112230-1779230.png',
+                        }}
+                        style={styles.banner}
+                        resizeMode="contain"
                     />
-                <Text style={styles.option}>Option 1</Text>
+                    <Text style={styles.option}>Option 1</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.optionButton}>
-                <Image 
-                    source = {{
-                        uri:'https://cdni.iconscout.com/illustration/premium/thumb/giving-different-feedback-and-review-in-websites-2112230-1779230.png',
-                    
-                    }} 
-                    style = {styles.banner}
-                    resizeMode = "contain"
+                    <Image
+                        source={{
+                            uri: 'https://cdni.iconscout.com/illustration/premium/thumb/giving-different-feedback-and-review-in-websites-2112230-1779230.png',
+                        }}
+                        style={styles.banner}
+                        resizeMode="contain"
                     />
-                <Text style={styles.option}>Option 2</Text>
+                    <Text style={styles.option}>Option 2</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.optionButton}>
-                <Image 
-                    source = {{
-                        uri:'https://cdni.iconscout.com/illustration/premium/thumb/giving-different-feedback-and-review-in-websites-2112230-1779230.png',
-                    
-                    }} 
-                    style = {styles.banner}
-                    resizeMode = "contain"
+                    <Image
+                        source={{
+                            uri: 'https://cdni.iconscout.com/illustration/premium/thumb/giving-different-feedback-and-review-in-websites-2112230-1779230.png',
+                        }}
+                        style={styles.banner}
+                        resizeMode="contain"
                     />
-                <Text style={styles.option}>Option 3</Text>
+                    <Text style={styles.option}>Option 3</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.optionButton}>
-                <Image 
-                    source = {{
-                        uri:'https://cdni.iconscout.com/illustration/premium/thumb/giving-different-feedback-and-review-in-websites-2112230-1779230.png',
-                    
-                    }} 
-                    style = {styles.banner}
-                    resizeMode = "contain"
+                    <Image
+                        source={{
+                            uri: 'https://cdni.iconscout.com/illustration/premium/thumb/giving-different-feedback-and-review-in-websites-2112230-1779230.png',
+                        }}
+                        style={styles.banner}
+                        resizeMode="contain"
                     />
-                <Text style={styles.option}>Option 4</Text>
+                    <Text style={styles.option}>Option 4</Text>
                 </TouchableOpacity>
-                </View>
-            <View style={styles.bottom}>
-                    <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>PREVIOUS</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                    style={styles.button}>
-                    <Text style={styles.buttonText}>NEXT</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                    style={styles.button}>
-                    <Text style={styles.buttonText}>QUIT</Text>
-                    </TouchableOpacity>
             </View>
             
+            <View style={styles.bottom}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Kiểm tra</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -79,23 +66,22 @@ export default ImgQuestionsScreen
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 40,
         paddingHorizontal: 20,
         height: '100%',
-      },
-      top: {
-        marginVertical: 16,
-      },
-      questions: {
-        fontSize: 28,
-      },
-      banner: {
-        height: 250,
-        width :170,
     },
-    options:{
+    top: {
+        marginVertical: 16,
+    },
+    questions: {
+        fontSize: 28,
+    },
+    banner: {
+        height: height / 4.5,
+        width: 170,
+    },
+    options: {
         display: 'flex',
-        flexWrap : 'wrap',
+        flexWrap: 'wrap',
         flexDirection: 'row',
         justifyContent: 'space-between'
     },
@@ -114,18 +100,19 @@ const styles = StyleSheet.create({
         marginVertical: 16,
         justifyContent: 'space-between',
         flexDirection: 'row',
-      },
-      button: {
+    },
+    button: {
+        flex: 1,
         backgroundColor: '#3399CC',
         padding: 16,
         borderRadius: 5,
         alignItems: 'center',
         marginBottom: 30,
         textAlign: 'center',
-      },
-      buttonText: {
+    },
+    buttonText: {
         fontSize: 18,
         fontWeight: '400',
         color: 'white',
-      },
+    },
 })
