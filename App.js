2a3b93e636d9ed3f51e1e23ac252acb347d84b77
shidokun557 <shidokun557@gmail.com>
2021-10-20 17:14:58 +0700

    push popup today

--- App.js
+++ App.js
@@ -12,15 +12,59 @@ import { NavigationContainer } from '@react-navigation/native';
 import MyTabs from './screens/MyTabs';
 import GlobalContextProvider from './context/GlobalContext';
 
+import { SafeAreaView,
+          StyleSheet,
+          StatusBar,
+          Text,
+          TouchableWithoutFeedback
+} from 'react-native'
+import { BottomPopup } from './screens/BottomPopup'
+
+
+
 const App = () => {
+  let popupRef = React.createRef()
+
+  const onShowPopup = () => {
+    popupRef.show()
+  }
+  const onClosePopup = () => {
+    popupRef.close()
+  }
   return (
+    <>
     <GlobalContextProvider>
       <NavigationContainer>
         <MyTabs />
       </NavigationContainer>
     </GlobalContextProvider>
-  );
-};
 
+      <StatusBar barStyle="dark-content"/>
+      <SafeAreaView style={styles.container}>
+        <TouchableWithoutFeedback onPress={onShowPopup}>
+          <Text style={styles.txtSize}> Test Show Popup</Text>
+        </TouchableWithoutFeedback>
+        <BottomPopup
+          title="Demo Popup"
+          ref={(target) => popupRef = target}
+          onTouchOutside={onClosePopup}
+          />
+      </SafeAreaView>
+
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex:1,
+      alignItems: "center",
+      justifyContent:"center"
+  },
+    txtSize: {
+      fontSize: 20
+    }
+      
+})
 
 export default App;
---
+++ screens/BottomPopup.js
@@ -0,0 +1,74 @@
+import {Modal, Disensions, TouchableWithoutFeedback, StyleSheet , View , Text, Dimensions} 
+from 'react-native'
+import React from 'react'
+
+const deviceHeight = Dimensions.get("window").height
+export class BottomPopup extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            show: false
+        }
+    }
+
+    show = () => {
+        this.setState({show: true})
+    }
+    close = () =>{
+        this.setState({show: flase})
+    }
+    renderOutsideTouchable (onTouch) {
+        const view = <View style = {{flex: 1, width:'100%'}}/>
+        if (!onTouch) return view
+
+        return(
+            <TouchableWithoutFeedback onPress={onTouch} style={{flex:1, width:'100%'}}>
+                {view}
+                </TouchableWithoutFeedback>
+        )
+    }
+
+    render(){
+        let {show} = this.state
+        const {onTouchOutside, title} = this.props
+
+        return(
+            <Modal
+                animationType={'fade'}
+                transparent={true}
+                visible={show}
+                onRequestClose={this.close}
+            >
+                <View style={{
+                    flex:1,
+                    backgroundColor: '#000000AA',
+                    justifyContent: 'flex-end'}}
+                >
+                    {this.renderOutsideTouchable(onTouchOutside)}
+                    <View style={{
+                        backgroundColor:'#FFFFFF',
+                        width:'100%',
+                        borderTopRightRadius: 10,
+                        borderTopLeftRadius: 10,
+                        paddingHorizontal: 10,
+                        maxHeight: deviceHeight * 0.4
+                    }}>
+                        <View>
+                            <Text style={{
+                                color:'#182E44',
+                                fontSize:20,
+                                fontWeight: '500',
+                                margin:15
+                                }}>
+                                {title}
+                            </Text>
+                        </View>
+
+                    </View>
+
+                </View>
+
+            </Modal>
+        )
+    }
+}
