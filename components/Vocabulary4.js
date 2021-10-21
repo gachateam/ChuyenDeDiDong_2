5b0d81c0ac68a79ec104b3d9bc14291054b9a3fc
baobaote00 <baobaote00@gmail.com>
2021-10-21 21:41:30 +0700

    :heart:

--- components/Vocabulary4.js
+++ components/Vocabulary4.js
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: '#3399CC',
     padding: 16,
-    borderRadius: 5,
+    borderRadius: 8,
     alignItems: 'center',
     marginBottom: 30,
     textAlign: 'center',
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import {NavigationContainer} from '@react-navigation/native';
-import {DrawerContent} from './DrawerContent';
+import { Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { DrawerContent } from './DrawerContent';
 import StageScreen from './StageScreen';
-import {useGlobal} from '../context/GlobalContext';
+import { useGlobal } from '../context/GlobalContext';
 import SplashScreen from './SplashScreen';
 import QuestionScreen from './QuestionScreen';
 
-function HomeScreens({navigation}) {
+function HomeScreens({ navigation }) {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>123</Text>
     </View>
   );
@@ -18,8 +18,8 @@ function HomeScreens({navigation}) {
 
 const Drawer = createDrawerNavigator();
 
-const HomeScreen = () => {
-  const {title} = useGlobal();
+const HomeScreen = ({ navigation }) => {
+  const { title } = useGlobal();
   return (
     <NavigationContainer independent={true}>
       <Drawer.Navigator
@@ -29,17 +29,17 @@ const HomeScreen = () => {
         <Drawer.Screen
           name="StageScreen"
           component={StageScreen}
-          options={{title: title}}
+          options={{ title: title }}
         />
         <Drawer.Screen
           name="SplashScreen"
           component={SplashScreen}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
         />
         <Drawer.Screen
           name="QuestionScreen"
           component={QuestionScreen}
-          options={{title: 'question'}}
+          options={{ title: 'question', headerShown: false }}
         />
       </Drawer.Navigator>
     </NavigationContainer>
