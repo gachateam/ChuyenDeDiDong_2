ab59a1c78cfc91a4b73fc29cd9cd9ed917a3d86b
baobaote00 <baobaote00@gmail.com>
2021-10-27 15:25:52 +0700

    finish merge

--- App.js
+++ App.js
@@ -12,61 +12,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import MyTabs from './screens/MyTabs';
 import GlobalContextProvider from './context/GlobalContext';
 
-import { SafeAreaView,
-          StyleSheet,
-          StatusBar,
-          Text,
-          TouchableWithoutFeedback
-} from 'react-native'
-import { BottomPopup } from './screens/BottomPopup'
-
-const popupList = [
-  {
-    id:1,
-    name:'Task'
-  },
-  {
-    id:2,
-    name:'Message'
-  },
-  {
-    id:3,
-    name:'Note'
-  },
-]
-
-
-
 const App = () => {
-  let popupRef = React.createRef()
-
-  const onShowPopup = () => {
-    popupRef.show()
-  }
-  const onClosePopup = () => {
-    popupRef.close()
-  }
   return (
-    <>
     <GlobalContextProvider>
       <NavigationContainer>
         <MyTabs />
       </NavigationContainer>
-    </GlobalContextProvider>     
-    </>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-      flex:1,
-      alignItems: "center",
-      justifyContent:"center"
-  },
-    txtSize: {
-      fontSize: 20
-    }
-      
-})
+    </GlobalContextProvider>
+  );
+};
 
 export default App;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,47 +7,44 @@ import StageScreen from './StageScreen';
 import {useGlobal} from '../context/GlobalContext';
 import SplashScreen from './SplashScreen';
 import QuestionScreen from './QuestionScreen';
-import { BottomPopup } from './BottomPopup';
+import {BottomPopup} from './BottomPopup';
 
-function HomeScreens({navigation}) {
-
-  let popupRef = React.createRef()
-
-  const onShowPopup = () => {
-    popupRef.show()
-  }
-  const onClosePopup = () => {
-    popupRef.close()
-  }
-  
 const popupList = [
   {
-    id:1,
-    name:'Task'
+    id: 1,
+    name: 'Task',
   },
   {
-    id:2,
-    name:'Message'
+    id: 2,
+    name: 'Message',
   },
   {
-    id:3,
-    name:'Note'
+    id: 3,
+    name: 'Note',
   },
-]
-  
+];
+function HomeScreens({navigation}) {
+  let popupRef = React.createRef();
+
+  const onShowPopup = () => {
+    popupRef.show();
+  };
+  const onClosePopup = () => {
+    popupRef.close();
+  };
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text onPress={onShowPopup}>123</Text>
       <BottomPopup
-          title="YOU đã chọn đúng , tiếp tục phát huy đi ...."
-          ref={(target) => popupRef = target}
-          onTouchOutside={onClosePopup}
-          data={popupList}
-          />
+        title="YOU đã chọn đúng , tiếp tục phát huy đi ...."
+        ref={target => (popupRef = target)}
+        onTouchOutside={onClosePopup}
+        data={popupList}
+      />
     </View>
   );
 }
-
 
 const Drawer = createDrawerNavigator();
 
