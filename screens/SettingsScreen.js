2db776260c3fc878ba3d1e6fb9af0fcdc57f8392
quangmanucians <68055977+quangmanucians@users.noreply.github.com>
2021-10-31 17:35:58 +0700

    Setting chưa xong :<

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { color } from 'react-native-reanimated';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const SettingsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>test</Text>
+      <View style={styles.top}>
+        <Text style={styles.txtTitle}>Settings</Text>
+      </View>
+       <ScrollView style = {styles.SelectionView}>
+            <TouchableOpacity style={styles.Selection}>
+              <Text style={styles.text}>My Info</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.Selection}>
+              <Text style={styles.text}>Account</Text>
+            </TouchableOpacity >
+            <TouchableOpacity style={styles.Selection}>
+              <Text style={styles.text}>Dark Mode</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.Selection}>
+              <Text style={styles.text}>Language</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.Selection}>
+              <Text style={styles.text}>LogOut</Text>
+            </TouchableOpacity>
+       </ScrollView>
+    
     </View>
   );
 };
@@ -12,9 +35,31 @@ const SettingsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 20,
+  },
+  txtTitle: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  top: {
+    marginVertical: 15,
+    textAlign: 'center',
     justifyContent: 'center',
     alignItems: 'center',
+  }, 
+  text: {
+    fontSize: 20,
   },
+  SelectionView: {
+    backgroundColor:color.grey,
+    height: 0.5,
+  }, 
+  Selection: {
+    flex: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  }
+
 });
 
 export default SettingsScreen;
