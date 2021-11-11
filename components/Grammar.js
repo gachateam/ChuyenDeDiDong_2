13d9739fa8346e08759faf4bef589da880dc8f01
baobaote00 <baobaote00@gmail.com>
2021-11-11 19:50:01 +0700

    :heart:

--- components/Grammar.js
+++ components/Grammar.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import FourChoice from './FourChoice';
 import QuestionBoxFillWord from './QuestionBoxFillWord';
+import database from '@react-native-firebase/database';
 
 const Grammar = ({ navigation }) => {
+
+    database()
+        .ref('/category/animal/easy/question/stage1/unit/')
+        .on('value', snapshot => {
+            console.log('User data: ', snapshot.val());
+        });
+        
     const question = {
         question: "Tiger is stronger ... zebra",
         meanQuestion: "Con sói hú vào lúc trăng tròn",
