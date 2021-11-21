bca6a09b9374d80eee2e9cf20b518beb97516c9a
Yamikur0 <68624487+Yamikur0@users.noreply.github.com>
2021-11-22 00:14:14 +0700

    update db, not done editprofile

--- screens/Profile/ForgotPasswordScreen.js
+++ screens/Profile/ForgotPasswordScreen.js
@@ -176,7 +176,7 @@ const styles = StyleSheet.create({
   header: {
     flex: 1,
     paddingHorizontal: 20,
-    paddingBottom: 50,
+    paddingBottom: 70,
     justifyContent: 'center',
     alignItems: 'center',
   },
