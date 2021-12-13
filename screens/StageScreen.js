3ce7d1939e17ae4c776d76cdab91358b5f46a122
baobaote00 <baobaote00@gmail.com>
2021-12-14 05:01:58 +0700

    Update StageScreen.js

--- screens/StageScreen.js
+++ screens/StageScreen.js
@@ -39,7 +39,7 @@ const StageScreen = ({navigation}) => {
         }
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [title]);
 
   const handleScroll = e => {
     dispatch({
